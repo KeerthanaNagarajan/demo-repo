8cf0370d260c584c90c50068efe919f8fd772022
Keerthana Nagarajan <42768946+KeerthanaNagarajan@users.noreply.github.com>
2023-01-25 15:53:49 -0500

    Update exponents.java
    commented a statement

--- exponents.java
+++ exponents.java
@@ -1,6 +1,6 @@
 class exponents{
     public static void main(String[] args){
-      System.out.println(Math.pow(8,3));
+      //System.out.println(Math.pow(8,3));
       int a=0;
       while(a<5)
         {
