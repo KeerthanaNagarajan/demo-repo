230bcd89a51e41d3a4d52f6b108ca747d10c683b
Keerthi <keerthananagarajan0106@gmail.com>
2023-01-25 15:52:07 -0500

    first java file

---
+++ exponents.java
@@ -0,0 +1,11 @@
+class exponents{
+    public static void main(String[] args){
+      System.out.println(Math.pow(8,3));
+      int a=0;
+      while(a<5)
+        {
+          System.out.println("the number is" + a );
+          a = a+1;
+        }
+    }
+  }
